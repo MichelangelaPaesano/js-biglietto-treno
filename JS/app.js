426d6d65f45af6113e6c86cc8cf00eef47a42809
MichelangelaPaesano <paesanomichelangela@gmail.com>
2025-05-08 11:51:07 +0200

    inserimento condizioni

--- JS/app.js
+++ JS/app.js
@@ -6,4 +6,14 @@ const km= parseInt(prompt("Inserisci il numero dei km"));
 let finalprice; //uso let perchè può cambiare e non assegniamo niente per ora
 
 //adesso iniziamo con le condizioni
+if (eta < 18) {
+    finalprice = prezzobase * km * 0.2;
+    console.log (`${finalprice.toFixed(2)}`)
+} else if (eta > 65) {
+    finalprice = prezzobase * km * 0.6;
+    console.log (`${finalprice.toFixed(2)}`)
+} else {
+    finalprice = prezzobase * km;
+    console.log (`${finalprice.toFixed(2)}`)
+}
 
