d36ad413da2ffde9082202b69572a17c41b7de24
MichelangelaPaesano <paesanomichelangela@gmail.com>
2025-05-08 11:37:00 +0200

    aggiunta variabili

--- JS/app.js
+++ JS/app.js
@@ -1,4 +1,9 @@
 //iniziamo a vedere come fare per creare le due variabili
 
 const eta= parseInt(prompt("Inserisci la tua età"));
+const prezzobase = 0.21;
 const km= parseInt(prompt("Inserisci il numero dei km"));
+let finalprice; //uso let perchè può cambiare e non assegniamo niente per ora
+
+//adesso iniziamo con le condizioni
+
